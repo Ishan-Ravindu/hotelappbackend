48acf5e0ee6f24448fe6a02b50b67b7cf6f72520
ishanRavindu <ishan@pop-os.localdomain>
2020-12-30 19:31:22 +0530

    .

--- hotelappbackend/settings.py
+++ hotelappbackend/settings.py
@@ -138,9 +138,6 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
-# MEDIA_URL = '/media/'
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
 CLOUDINARY_STORAGE = {
     'CLOUD_NAME': 'hbnmovn6m',
     'API_KEY': '734692948537426',
