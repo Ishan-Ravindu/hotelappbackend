9d26713a1de9e58811ef5f9994e2d9e20b5ce767
ishanRavindu <ishan@pop-os.localdomain>
2020-12-30 13:25:08 +0530

    change

--- hotelappbackend/settings.py
+++ hotelappbackend/settings.py
@@ -87,6 +87,8 @@ WSGI_APPLICATION = 'hotelappbackend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': 'mydatabase',
 
     }
 }
