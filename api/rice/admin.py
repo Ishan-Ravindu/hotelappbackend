09caae1a8cafafc00d5ac6e9f5b9cc7d51a7df1d
ishanRavindu <ishan@pop-os.localdomain>
2021-01-08 14:51:42 +0530

    change admin in rice model

--- api/rice/admin.py
+++ api/rice/admin.py
@@ -6,6 +6,20 @@ class curry(admin.ModelAdmin):
     list_display = ("id", "name", "price",
                     "is_stock_avalable")
 
+    list_filter = ("is_stock_avalable",)
+    search_fields = ("name",)
+    actions = ("stock_avalable", "stock_not_avalable")
+
+    def stock_avalable(self, request, queryset):
+        count = queryset.update(is_stock_avalable=True)
+        self.message_user(
+            request, "{} filds are changed to stock avalable ".format(count))
+
+    def stock_not_avalable(self, request, queryset):
+        count = queryset.update(is_stock_avalable=False)
+        self.message_user(
+            request, "{} filds are changed to stock not avalable ".format(count))
+
 
 admin.site.register(Curry, curry)
 
@@ -13,6 +27,20 @@ admin.site.register(Curry, curry)
 class rice(admin.ModelAdmin):
     list_display = ("id", "name", "price",
                     "is_stock_avalable")
+
+    list_filter = ("is_stock_avalable", )
+    search_fields = ("name",)
+    actions = ("stock_avalable", "stock_not_avalable")
+
+    def stock_avalable(self, request, queryset):
+        count = queryset.update(is_stock_avalable=True)
+        self.message_user(
+            request, "{} filds are changed to stock avalable ".format(count))
+
+    def stock_not_avalable(self, request, queryset):
+        count = queryset.update(is_stock_avalable=False)
+        self.message_user(
+            request, "{} filds are changed to stock not avalable ".format(count))
 
 
 admin.site.register(Rice, rice)
