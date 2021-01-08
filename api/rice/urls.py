2e35a613adad5a4b286b2f40669f785503522fa7
ishanRavindu <ishan@pop-os.localdomain>
2021-01-08 14:34:06 +0530

    .

--- api/rice/urls.py
+++ api/rice/urls.py
@@ -2,8 +2,6 @@ from rest_framework import routers
 from django.urls import path, include
 from . import views
 
-imageRouter = routers.DefaultRouter()
-imageRouter.register('image', views.ImageViewSet)
 
 riceRouter = routers.DefaultRouter()
 riceRouter.register('rice', views.RiceViewSet)
@@ -12,7 +10,7 @@ curryRouter = routers.DefaultRouter()
 curryRouter.register('curry', views.CurryViewSet)
 
 urlpatterns = [
-    path('', include(imageRouter.urls)),
+
     path('', include(riceRouter.urls)),
     path('', include(curryRouter.urls)),
 ]
